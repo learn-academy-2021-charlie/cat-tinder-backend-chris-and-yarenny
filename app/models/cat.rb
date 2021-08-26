a4d345dbd291531b7016c8217c09cfb0772a006b
christophermackey <c.mackey@me.com>
2021-08-26 16:42:38 -0700

    fixes length validation

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,5 +1,4 @@
 class Cat < ApplicationRecord
-    validates :name, presence: true
-    validates :age, presence: true
-    validates :enjoys, presence: true, length: { minimum: 10 }
+    validates :name, :age, :enjoys, presence: true
+    validates :enjoys, length: { minimum: 10 }
 end
